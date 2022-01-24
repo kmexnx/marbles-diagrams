084d6bde85746ce0ca1267bb425984ba08e8c2a9
Carlos Arenas <kmexnx@outlook.com>
2022-01-24 02:26:02 -0600

    Test fromEvent

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { from, take } from 'rxjs';
+import { from, fromEvent, take } from 'rxjs';
 
 import './style.css'
 
@@ -7,23 +7,33 @@ const app = document.querySelector<HTMLDivElement>('#app')!
 app.innerHTML = `
   <h1>Marble Reactive Diagrams!</h1>
   <div class="container"></div>
+  <div class="sum"></div>
 `;
 
-const addMarble = (val: number | string) => {
-  const el = app.querySelector('.container')!.appendChild(document.createElement('div'));  
+let val = 0;
+const addMarble = (val: number | string, element?: string) => {
+  const el = app.querySelector(element ? element : '.container')!.appendChild(document.createElement('div'));  
   el.setAttribute('class','marble');
-  el.append(`${val}`);
+  el.appendChild(document.createElement('div')).append(`${val}`);
 }
 
 const arr = Array(10).fill(null).map(() => Math.floor(Math.random() * 100));
 
-const observable = from(arr).pipe(take(5));
+const observable = from(arr).pipe(take(10));
 
 const subscription = observable.subscribe({
   next(x) {
     addMarble(x);
   }
 });
+
+const click$ = fromEvent(app.querySelectorAll('.marble')!, 'click');
+
+click$.subscribe(x => {
+  const el = x.target as HTMLDivElement;
+  addMarble(val = val + Number(el.innerText), '.sum');
+});
+
 subscription.unsubscribe();
 console.log('After suscribe');
 
